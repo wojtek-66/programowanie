1394b1663c9e45bfcdfe6ee97bdacadaf771f597
Wojtek <wojciech66@op.pl>
2026-01-25 18:41:09 +0100

    zmiana programu ikona

---
+++ programy/ikony/ikona/app.py
@@ -0,0 +1,9 @@
+import sys
+
+def ikonka(parametr: str):
+# 1. Przypisać parametr do zmiennej (wczytać parametr)
+    parametr = sys.argv[1]
+    parametr = ":-", parametr , ":-"
+
+# 2. Wyspisac parametr z ikonami
+    return parametr
